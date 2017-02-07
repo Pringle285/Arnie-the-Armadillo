6c900bbfee292a79e6514ffaff2cf3eb4cf89a5c
Grigler <graham.rigler@gmail.com>
2017-02-07 14:32:48 +0000

    Smooth Follow Camera

--- Arnie/Assets/Scripts/Arnie/CameraFollowArnie.cs
+++ Arnie/Assets/Scripts/Arnie/CameraFollowArnie.cs
@@ -5,19 +5,23 @@ public class CameraFollowArnie : MonoBehaviour {
 	[Range(0.0f, 10.0f)]
 	public float m_cameraFollowAlpha;
 
-	private GameObject m_lerpTarget;
+	private GameObject m_cameraTarget;
 
 	private GameObject m_player;
 	private Vector3 m_lastPlayerPos;
 
+	private Vector3 m_tempVec = Vector3.zero;
+
+	//private Transform targetTrans = new Transform();
 
 	private float m_xRot;
 
 	void Start () 
 	{
-		m_lerpTarget = GameObject.Find ("CameraPos");
+		m_cameraTarget = GameObject.Find ("CameraTarget");
 		m_player = GameObject.FindGameObjectWithTag ("Player");
 
+		m_cameraTarget.transform.position = this.transform.position;
 
 		Cursor.lockState = CursorLockMode.Confined;
 		#if UNITY_EDITOR
@@ -36,13 +40,18 @@ public class CameraFollowArnie : MonoBehaviour {
 
 	void LateUpdate ()
 	{
-		//this.transform.position = Vector3.MoveTowards (this.transform.position, m_lerpTarget.transform.position, m_cameraFollowAlpha*Time.deltaTime);
-
+		Quaternion tempQ = this.transform.rotation;
 		this.transform.LookAt (m_player.transform.position);
+		Quaternion targetRotation = this.transform.rotation;
+		this.transform.rotation = tempQ;
 
-		//TODO make wayyyyyy less jittery
+		this.transform.rotation = Quaternion.Slerp (tempQ, targetRotation, 0.5f * Time.deltaTime);
+
 		Vector3 deltaPos = new Vector3(m_player.transform.position.x - m_lastPlayerPos.x, 0, m_player.transform.position.z - m_lastPlayerPos.z);
-		this.transform.position += deltaPos;
+
+		m_cameraTarget.transform.position += deltaPos;
+
+		this.transform.position = Vector3.SmoothDamp (this.transform.position, m_cameraTarget.transform.position, ref m_tempVec, 30.0f * Time.deltaTime);
 
 		m_lastPlayerPos = m_player.transform.position;
 
@@ -52,5 +61,7 @@ public class CameraFollowArnie : MonoBehaviour {
 	void RotateAroundArnie(float _angle)
 	{
 		this.transform.RotateAround (m_player.transform.position, Vector3.up, _angle * Time.deltaTime);
+
+		m_cameraTarget.transform.RotateAround (m_player.transform.position, Vector3.up, _angle * Time.deltaTime);
 	}
 }
