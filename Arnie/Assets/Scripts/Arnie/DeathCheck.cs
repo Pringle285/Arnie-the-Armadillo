45d9efadad20be5c69790a4a18638df91bd60dd9
Grigler <graham.rigler@gmail.com>
2017-02-28 14:37:18 +0000

    Start of Death Checks

---
+++ Arnie/Assets/Scripts/Arnie/DeathCheck.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathCheck : MonoBehaviour {
+
+	public static GameObject m_checkpoint;
+	public static bool m_isDying = false;
+
+	public GameObject m_player;
+
+	void Start()
+	{
+		m_player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "Enemy")
+		{
+			KillArnie ();
+		}
+	}
+
+	public void KillArnie()
+	{
+		if(m_isDying)
+		{
+			//Check if death animation is done
+			//	if it is then restart scene at checkpoint
+		}
+		else
+		{
+			//Trigger animation
+
+			//Setting isDying to true
+			m_isDying = true;
+
+			//Stopping timeScale so that game seems to 'pause' for Arnie's death
+			Time.timeScale = 0f;
+		}
+
+	}
+}
