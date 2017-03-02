cf35d7e4d139a18042805e330231c7497e738e14
Grigler <graham.rigler@gmail.com>
2017-03-02 15:54:20 +0000

    Unity Audio Mix for Lvl-1, using placeholders

---
+++ Arnie/Assets/Audio/Level 1/AudioControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class AudioControl : MonoBehaviour {
+
+	public AudioMixer m_mix;
+	public AudioSource m_spike;
+
+	//TODO Remove update function, it's purely for testing
+	void Update()
+	{
+		if (Input.GetKey (KeyCode.J))
+			TransitionToChase ();
+		else if (Input.GetKey (KeyCode.K))
+			TransitionToCalm ();
+	}
+
+	public void TransitionToChase()
+	{
+		//Play spike as one-shot
+		m_spike.Play();
+		//Quickly transition to Chase Snapshot
+		m_mix.FindSnapshot("Chase").TransitionTo(0.5f);
+	}
+	public void TransitionToCalm()
+	{
+		m_mix.FindSnapshot ("Calm").TransitionTo (1f); //TODO time will need editing
+	}
+}
