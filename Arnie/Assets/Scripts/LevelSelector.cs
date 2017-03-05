558ca644928079b03eaf020e2124148a33b89e00
pringle285 <rhys.male@blueyonder.co.uk>
2017-03-05 11:24:25 +0000

    level changing scripts and end burrow trigger lvl1

---
+++ Arnie/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; 
+
+public class LevelSelector : MonoBehaviour {
+
+	public void LevelSelect()
+	{
+		if (SceneManager.GetActiveScene ().name == "Level 1 first half whitebox")
+			SceneManager.LoadScene ("Level2");
+		else if (SceneManager.GetActiveScene ().name == "Level2")
+			SceneManager.LoadScene ("Level3");
+	}
+
+
+}
---
+++ Arnie/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class NextLevel : MonoBehaviour {
+
+	public Transform LevelCompletePanel; 
+
+	void OnTriggerEnter (Collider other)
+	{
+		LevelCompletePanel.gameObject.SetActive (true);
+		Time.timeScale = 0; 
+	}
+}
