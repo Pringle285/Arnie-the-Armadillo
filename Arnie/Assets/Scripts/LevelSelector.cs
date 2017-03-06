d48c00c95948f05695b9c4e441e0ee8dbf5b7a02
pringle285 <rhys.male@blueyonder.co.uk>
2017-03-06 10:44:21 +0000

    cursor shit

--- Arnie/Assets/Scripts/LevelSelector.cs
+++ Arnie/Assets/Scripts/LevelSelector.cs
@@ -6,10 +6,12 @@ public class LevelSelector : MonoBehaviour {
 
 	public void LevelSelect()
 	{
+		
 		if (SceneManager.GetActiveScene ().name == "Level 1 first half whitebox")
 			SceneManager.LoadScene ("Level2");
 		else if (SceneManager.GetActiveScene ().name == "Level2")
 			SceneManager.LoadScene ("Level3");
+		Cursor.visible = false; 
 	}
 
 
